ad5ce22ed327546bb52618ecdc1f7083c0952c98
= <=>
2020-04-03 01:47:23 +0900

    add redirect func

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import { lemonchiffon } from 'color-name';
 
 Vue.use(VueRouter)
 
@@ -18,6 +19,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/redirect', redirect: to => {
+      // console.log(to.query.url)
+      window.location = to.query.url
+      liff.closeWindow()
+    }
   }
 ]
 
